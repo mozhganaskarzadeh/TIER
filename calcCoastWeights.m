463e762b109e5c009d2bedf0ee2cf6d301420576
Andy Newman <anewman@ucar.edu>
2019-03-21 18:24:31 +0000

    Fix to prevent divide by zero for coastal weights if grid and station distances are the same

--- calcCoastWeights.m
+++ calcCoastWeights.m
@@ -18,8 +18,11 @@ function coastWeights = calcCoastWeights(gridDistanceToCoast,stationDistanceToCo
 %   coastWeights, float, vector holding coastal weights for nearby stations
 %
 
+    %define a tiny float
+    tiny = 1e-5;
+
     %distance to coast weighting (e.g. Daly et al. 2002)
-    coastWeights = 1.0./(abs(gridDistanceToCoast-stationDistanceToCoast)).^(0.75);
+    coastWeights = 1.0./(abs(gridDistanceToCoast-stationDistanceToCoast)+tiny).^(0.75);
     %check for values > 1
     coastWeights(coastWeights>1) = 1.0;
     %normalize weights

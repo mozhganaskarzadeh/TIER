862954b4abd4e735f5b92238daaffdb27d9b118d
Andy Newman <anewman@ucar.edu>
2019-04-04 17:26:49 +0000

    Correctly read in grid dx dimension and convert to km

--- stirModel/readGrid.m
+++ stirModel/readGrid.m
@@ -40,7 +40,29 @@ function grid = readGrid(gridName)
     grid.lat = ncread(gridName,'latitude');
     grid.lon = ncread(gridName,'longitude');
     %grid spacing
-    grid.dx = ncread(gridName,'dx')/1000; %convert m to km
+    gridUnits = ncreadatt(gridName,'dx','units');
+    if(strcmpi(gridUnits,'degrees'))
+        grid.dx = ncread(gridName,'dx');
+        %convert to km (roughly)
+        
+        %grid lower left lat,lon
+        startLon = ncread(gridName,'startx'); 
+        startLat = ncread(gridName,'starty');
+
+        %find grid point distance in km approximately
+        gridDist = distance(startLat,startLon,startLat+grid.dx,startLon+grid.dx);
+        kmPerLat = rad2km(deg2rad(gridDist));
+        
+        %grid distance in km roughly
+        grid.dx = (grid.dx*kmPerLat)/sqrt(gridDist.^2);
+        
+    elseif(strcmpi(gridUnits,'km'))
+        grid.dx = ncread(gridName,'dx');
+    elseif(strcmpi(gridUnits,'m'))
+        grid.dx = ncread(gridName,'dx')/1000; %convert m to km
+    else
+        error('Unknown grid dx units: %s\n',gridUnits);
+    end
     %read valid grid point mask
     grid.mask = ncread(gridName,'mask');
     %read DEM
